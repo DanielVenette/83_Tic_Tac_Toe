b540f249f0a9bb106c18e2b1c9703c679aff755e
DanielVenette <danvenette@gmail.com>
2023-06-01 16:35:44 -0400

    Added all winning combinations.  Added cat's game logic and message.  Added option to play again.

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 from winner import check_for_winner
 from gameboard import Gameboard
 
-# Create a game board
 
-x_spaces = []
-o_spaces = []
+def play_game():
+    x_spaces = []
+    o_spaces = []
 
-turn_tracker = 1
-is_winner = False
+    turn_tracker = 1
+    is_winner = False
 
-gameboard = Gameboard()
-
-gameboard.show_gameboard()
-
-while not is_winner:
-
-    if turn_tracker % 2 == 0:
-        player = "O"
-    else:
-        player = "X"
-
-    chosen_space = gameboard.choose_space(player, x_spaces, o_spaces)
-    gameboard.update_gameboard(chosen_space, player)
+    # Create a game board
+    gameboard = Gameboard()
     gameboard.show_gameboard()
 
-    is_winner = check_for_winner(x_spaces, o_spaces)
+    while not is_winner:
 
-    turn_tracker += 1
+        if turn_tracker > 9:
+            print("Cat's game!")
+            break
+        elif turn_tracker % 2 == 0:
+            player = "O"
+        else:
+            player = "X"
+
+        chosen_space = gameboard.choose_space(player, x_spaces, o_spaces)
+        gameboard.update_gameboard(chosen_space, player)
+        gameboard.show_gameboard()
+
+        is_winner = check_for_winner(x_spaces, o_spaces)
+
+        turn_tracker += 1
+
+
+keep_playing = True
+
+while(keep_playing):
+    play_game()
+
+    while True:
+        user_selection = input("Would you like to play again? (Y/N): ")
+        if user_selection == "Y" or user_selection == "y":
+            keep_playing = True
+            break
+        elif user_selection == "N" or user_selection == "n":
+            keep_playing = False
+            break
+        else:
+            print("Please enter a valid response")
 
 print("Thanks for playing!")
 
--- winner.py
+++ winner.py
@@ -3,10 +3,11 @@ def check_for_winner(x_spaces, o_spaces):
 
     winning_combinations = [
         [1, 2, 3],
-        [1, 4, 7],
-        [3, 6, 9],
-        [7, 8, 9],
         [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
         [1, 5, 9],
         [3, 5, 7]
     ]
@@ -15,9 +16,11 @@ def check_for_winner(x_spaces, o_spaces):
         if all(space in x_spaces for space in combination):
             print("X is the winner")
             winner_exists = True
+            break
         if all(space in o_spaces for space in combination):
             print("O is the winner")
             winner_exists = True
+            break
 
     # print(f"X spaces: {x_spaces}")
     # print(f"O spaces: {o_spaces}")
